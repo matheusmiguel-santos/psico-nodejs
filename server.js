5c9a44711abc099786456e7a6c57df858eb7478d
qualityseg <140438431+MmSTRoK-br@users.noreply.github.com>
2023-07-25 14:16:22 -0300

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ function handleDisconnect() {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: 'Psico-qslib',
     connectionLimit: 10,
   });
 
