bc0eaff2a697e87057bfa7df6c012b6d99205981
qualityseg <140438431+MmSTRoK-br@users.noreply.github.com>
2023-07-25 07:33:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -16,28 +16,39 @@ const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: 'Psico-qslib',
   connectionLimit: 10,
 });
 
-db.getConnection((err, connection) => {
-  if (err) throw err;
-  console.log('Conectado ao banco de dados MySQL');
-  connection.release();
-});
 
 db.getConnection((err, connection) => {
   if (err) throw err;
   console.log('Conectado ao banco de dados MySQL');
+
   connection.query(
     "CREATE TABLE IF NOT EXISTS cadastro_clientes (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), surname VARCHAR(255), email VARCHAR(255), birthDate DATE, gender VARCHAR(10), phone VARCHAR(20), phone2 VARCHAR(20), cpf VARCHAR(14), cnpj VARCHAR(18), registration VARCHAR(255), obs TEXT, address VARCHAR(255), number VARCHAR(10), complement VARCHAR(255), district VARCHAR(255), city VARCHAR(255), state VARCHAR(255), country VARCHAR(255), zipCode VARCHAR(10), unit VARCHAR(255), sector VARCHAR(255), role VARCHAR(255), institution VARCHAR(255), accessRecovery BOOLEAN, access VARCHAR(50))",
     (err, result) => {
-      if (err) throw err;
+      if (err) {
+        console.log(err);
+        throw err;
+      }
       console.log('Tabela "cadastro_clientes" verificada/criada');
+    }
+  );
+
+  connection.query(
+    "CREATE TABLE IF NOT EXISTS login_register (id INT AUTO_INCREMENT PRIMARY KEY, usuario VARCHAR(255), senha VARCHAR(255))",
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        throw err;
+      }
+      console.log('Tabela "login_register" verificada/criada');
     }
   );
   connection.release();
 });
+
 
 app.use(cors());
 app.use(express.json());
@@ -108,19 +119,6 @@ app.post('/register', (req, res) => {
     }
     res.send({ success: true });
   });         
-});
-
-
-// Buscar todos os usuários
-app.get('/users', (req, res) => {
-  const query = 'SELECT * FROM cadastro_clientes';
-  db.query(query, (err, results) => {
-    if (err) {
-      console.log(err);
-      return res.send({ success: false, message: err.message });
-    }
-    res.send({ success: true, users: results });
-  });
 });
 
 // Buscar um usuário específico pelo seu ID
@@ -250,6 +248,31 @@ app.post('/login', (req, res) => {
     res.send({ success: true, username: user.usuario, role: user.acesso, token });
   });
 });
+
+app.post('/register_usuario', async (req, res) => {
+  const { usuario, nome, email, senha, unidade, setor, acesso } = req.body;
+
+  try {
+    // Criptografe a senha antes de armazenar no banco de dados
+    const senhaHash = await bcrypt.hash(senha, 10);
+
+    const query = 'INSERT INTO login_register (usuario, nome, email, senha, unidade, setor, acesso) VALUES (?, ?, ?, ?, ?, ?, ?)';
+    const values = [usuario, nome, email, senhaHash, unidade, setor, acesso];
+
+    db.query(query, values, (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.send({ success: false, message: err.message });
+      }
+      res.send({ success: true });
+    });
+  } catch (err) {
+    console.log(err);
+    return res.send({ success: false, message: err.message });
+  }
+});
+
+
 
 app.delete('/deleteAll', (req, res) => {
   const query = 'DELETE FROM cadastro_clientes';
